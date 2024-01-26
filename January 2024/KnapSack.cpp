8ede4abde79e6d9c0a5ed584b0cea444425ac83d
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2024-01-26 23:44:13 +0530

    Leetcode Consistency 26 Jan 2024

---
+++ January 2024/KnapSack.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    // Function to get the maximum total value in the knapsack.
+    static bool compare(Item &a, Item &b)
+    {
+        float aVal = 1.0 * a.value / a.weight;
+        float bVal = 1.0 * b.value / b.weight;
+        return aVal > bVal;
+    }
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        double ans = 0;
+        sort(arr, arr + n, compare);
+        for (int i = 0; i < n; i++)
+        {
+            int take = min(W, arr[i].weight);
+            double val = 1.0 * arr[i].value / arr[i].weight;
+            ans += take * val;
+            W -= take;
+        }
+        return ans;
+    }
+};
