29f0a4e7886b230a3438c9e45a07029eabb1f124
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2024-02-03 22:46:32 +0530

    LeetCode and GFG Consistency 03 Feb 2024

---
+++ February 2024/Decimal.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+    long long unsigned int decimalValue(Node *head) {
+        long long unsigned int decimal_value=0, M=1e9+7;
+        while(head) {
+            decimal_value = (decimal_value*2+head->data) % M;
+            head=head->next;
+        }
+        return decimal_value;
+    }
+};
