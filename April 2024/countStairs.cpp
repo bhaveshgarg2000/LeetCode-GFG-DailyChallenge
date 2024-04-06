cd038a6c01d68415acfbb9aaa457dee4bd6a9e04
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2024-04-06 19:26:45 +0530

    LeetCode & GFG Consistency 06 April 2024

---
+++ April 2024/countStairs.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    long long countWays(int n)
+    {
+        if (n % 2 == 0)
+        {
+            long long ans = n / 2;w
+            ans = ans + 1;
+            return ans;
+        }
+        else
+        {
+            n = n - 1;
+            long long ans = n / 2;
+            ans = ans + 1;
+            return ans;
+        }
+    }
+};
