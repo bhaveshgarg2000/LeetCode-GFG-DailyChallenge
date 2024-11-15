aa13cb387f9561da162c76d7abc395bb3888ccfd
BHAVESH KRISHAN GARG <168732086+df-bhavesh@users.noreply.github.com>
2024-11-15 10:38:02 +0530

    LeetCode & GFG 15-11-2024

---
+++ November 2024/1574.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int findLengthOfShortestSubarray(vector<int> &arr)
+    {
+        int n = arr.size();
+        int l = 0;
+        int r = n - 1;
+        while (r > 0 && arr[r] >= arr[r - 1])
+        {
+            r--;
+        }
+
+        int shortest = r;
+        while (l < r)
+        {
+            while (r < n && arr[l] > arr[r])
+            {
+                r++;
+            }
+            shortest = min(shortest, r - l - 1);
+            l++;
+            if (arr[l] < arr[l - 1])
+            {
+                break;
+            }
+        }
+        return shortest;
+    }
+};
